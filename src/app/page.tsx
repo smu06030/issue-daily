aa101f3d28f400bfbd5444ccb8e6a8a64477d38a
incheol <smu06030>
2024-10-10 17:42:04 +0900

    project: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-pretendard">
+      <div className="font-thin">dsfsdf</div>
+      <div className="font-bold">dsfsdf</div>
+      <div className="font-medium">dsfsdf</div>
+    </div>
+  );
+}
