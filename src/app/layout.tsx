a8194efec2e67c61b5f9aaacb2f372772cb09fb1
incheol <smu06030>
2024-10-10 17:54:08 +0900

    fix: 리액트 모듈 import 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import React from "react";
 
 const pretendard = localFont({
   src: "./fonts/PretendardVariable.woff2",
