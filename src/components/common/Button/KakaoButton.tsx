7c39a5dc798c2b90754355c42e20e12098ed0742
incheol <smu06030>
2024-10-14 13:59:47 +0900

    [#4] feat: 카카오 로그인 구현 및 리다이렉션 구성

--- src/components/common/Button/KakaoButton.tsx
+++ src/components/common/Button/KakaoButton.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+'use client';
+
+import browserClient from '@/utils/supabase/client';
 import Image from 'next/image';
 
 const KakaoButton = () => {
+  const signInWithKakao = async () => {
+    await browserClient.auth.signInWithOAuth({
+      provider: 'kakao',
+      options: {
+        redirectTo: window.origin + '/auth/callback'
+      }
+    });
+  };
+
   return (
     <button
       type="submit"
+      onClick={signInWithKakao}
       className="mt-3 flex h-[50px] w-[348px] items-center justify-center gap-4 rounded-md bg-[#FAE64C] font-bold text-black shadow-buttonShadow hover:bg-[#fded6f]"
     >
       <Image src="/kakao.png" alt="카카오" width={24} height={24} />
